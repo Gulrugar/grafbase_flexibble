0cd2bf463efe8acfb1f6cf8bfc7b4690c33bf96b
Andrew Aguirre <andrewa96@ymail.com>
2023-07-15 11:17:49 -0700

    second commit

--- lib/actions.ts
+++ lib/actions.ts
@@ -12,6 +12,8 @@ import {
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = true;
+
 const apiUrl = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
   : "http://127.0.0.1:4000/graphql";
